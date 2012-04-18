fcda6ae625511288905f6a7c11182f0ea4b69579
Linda <lin.m.gonzalez@gmail.com>
2012-04-17 21:19:02 -0400

    Restricting ticket updating to only those who have permission

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -6,6 +6,7 @@ class TicketsController < ApplicationController
                                         :update,
                                         :destroy]
   before_filter :authorize_create!, :only => [:new, :create]
+  before_filter :authorize_update!, :only => [:edit, :update]
   
   def new
     @ticket = @project.tickets.build
@@ -58,6 +59,7 @@ private
       flash[:alert] = "The project you were looking for could not be found."
       redirect_to root_path
   end
+end
 
   def find_ticket
     @ticket = @project.tickets.find(params[:id])
@@ -69,4 +71,10 @@ private
       redirect_to @project
     end
   end
-end
+  
+  def authorize_update!
+   if !current_user.admin? && cannot?(:"edit tickets", @project)
+      flash[:alert] = "You cannot edit tickets on this project."
+      redirect_to @project
+    end
+  end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,11 +3,8 @@ class Ability
   
   def initialize(user)
     user.permissions.each do |permission|
-      can permission.action.to_sym,
-       permission.thing_type.constantize do |thing|
-        thing.nil? ||
-        permission.thing_id.nil? ||
-        permission.thing_id == thing.id
+      can(permission.action.to_sym, permission.thing_type.constantize) do |thing|
+        thing.nil? || permission.thing_id.nil? || permission.thing_id == thing.id
       end
     end
   end
